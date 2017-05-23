bfac2788694d8338427a3c24ecce63865f98f51f
Astar <molot75@yandex.ru>
2017-05-23 11:32:03 +0300

    23/05/2017  11:31

--- app/src/main/java/ru/astar/fuckprefect/MainActivity.java
+++ app/src/main/java/ru/astar/fuckprefect/MainActivity.java
@@ -2,13 +2,93 @@ package ru.astar.fuckprefect;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView previewImage;
+    private Button prevButton;
+    private Button nextButton;
+    private Button saveButton;
+    private Button decrementMinuteButton;
+    private Button incrementMinuteButton;
+    private Button setDateButton;
+    private TextView settedDateTextView;
+    private TextView countImageTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
     }
 
+    private void initView() {
+        previewImage = (ImageView) findViewById(R.id.previewImage);
+        prevButton = (Button) findViewById(R.id.prevImageButton);
+        nextButton = (Button) findViewById(R.id.nextImageButton);
+        saveButton = (Button) findViewById(R.id.saveImageButton);
+        decrementMinuteButton = (Button) findViewById(R.id.decrementMinuteButton);
+        incrementMinuteButton = (Button) findViewById(R.id.incrementMinuteButton);
+        setDateButton = (Button) findViewById(R.id.setDateTimeButton);
+        settedDateTextView = (TextView) findViewById(R.id.settedDataTextView);
+        countImageTextView = (TextView) findViewById(R.id.countImageTextView);
+
+        ClickListener clickListener = new ClickListener();
+
+        previewImage.setOnClickListener(clickListener);
+        prevButton.setOnClickListener(clickListener);
+        nextButton.setOnClickListener(clickListener);
+        saveButton.setOnClickListener(clickListener);
+        decrementMinuteButton.setOnClickListener(clickListener);
+        incrementMinuteButton.setOnClickListener(clickListener);
+        setDateButton.setOnClickListener(clickListener);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.openDirectoryItem:
+
+                break;
+
+            case R.id.settingItem:
+
+                break;
+
+            case R.id.aboutItem:
+
+                break;
+
+            case R.id.exitItem:
+                finish();
+                break;
+        }
+
+        return true;
+    }
+
+    private class ClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()) {
+
+            }
+        }
+    }
 }
---
+++ app/src/main/java/ru/astar/fuckprefect/Photo.java
@@ -0,0 +1,8 @@
+package ru.astar.fuckprefect;
+
+/**
+ * Created by molot on 23.05.2017.
+ */
+
+public class Photo {
+}
