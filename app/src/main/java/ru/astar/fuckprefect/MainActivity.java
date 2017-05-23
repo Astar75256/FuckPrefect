d02fba003da45ecb079321fe90458574777ed354
Astar <molot75@yandex.ru>
2017-05-23 22:09:04 +0300

    Редактирование и сохранение фото

--- app/src/main/java/ru/astar/fuckprefect/MainActivity.java
+++ app/src/main/java/ru/astar/fuckprefect/MainActivity.java
@@ -6,7 +6,6 @@ import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
